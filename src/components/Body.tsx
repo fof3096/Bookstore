7c093f1f54b35daad7ed8155290ad29ebcdeccb7
fof3096 <c0rr1f1ll@gmail.com>
2024-11-11 19:46:52 -0300

    Desktop View InfoBar

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,10 +1,12 @@
 import Box from '@mui/material/Box'
 import Carousel from './Carousel'
+import InfoBar from './InfoBar'
 
 export default function Body() {
   return (
     <Box component={"main"} mx={"auto"} sx={{ maxWidth: 1300}}>
         <Carousel />
+        <InfoBar />
     </Box>
   )
 }
---
+++ src/components/InfoBar.tsx
@@ -0,0 +1,43 @@
+import { Box, Divider, Stack, Typography } from "@mui/material";
+import StorefrontIcon from '@mui/icons-material/Storefront';
+import LocalShippingOutlinedIcon from '@mui/icons-material/LocalShippingOutlined';
+import CreditCardIcon from '@mui/icons-material/CreditCard';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+
+export default function InfoBar() {
+  return (
+    <Stack p={2} direction={"row"} justifyContent={"space-evenly"} divider={<Divider orientation="vertical" flexItem sx={{ borderColor: "text.Secondary" }}/>}>
+      <Box display={"flex"} alignItems={"center"} gap={1}>
+        <StorefrontIcon />
+        <Stack spacing={-1}>
+          <Typography fontWeight={"bold"}>RETIRO GRATIS</Typography>
+          <Typography fontSize={"small"} color="textSecondary">en sucursal</Typography>
+        </Stack>
+      </Box>
+
+      <Box display={"flex"} alignItems={"center"} gap={1}>
+        <LocalShippingOutlinedIcon />
+        <Stack spacing={-1}>
+          <Typography fontWeight={"bold"}>ENVÍOS A DOMICIOLIO</Typography>
+          <Typography fontSize={"small"} color="textSecondary">en toda la provincia</Typography>
+        </Stack>
+      </Box>
+
+      <Box display={"flex"} alignItems={"center"} gap={1}>
+        <CreditCardIcon />
+        <Stack spacing={-1}>
+          <Typography fontWeight={"bold"}>PROMOS A BANCARIAS</Typography>
+          <Typography fontSize={"small"} color="textSecondary">y medios de pago</Typography>
+        </Stack>
+      </Box>
+
+      <Box display={"flex"} alignItems={"center"} gap={1}>
+        <WhatsAppIcon />
+        <Stack spacing={-1}>
+          <Typography fontWeight={"bold"}>223 3393007</Typography>
+          <Typography fontSize={"small"} color="textSecondary">consultanos</Typography>
+        </Stack>
+      </Box>
+    </Stack>
+  )
+}

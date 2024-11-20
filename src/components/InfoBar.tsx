ef5844f2ee5cee31533454c8a2bfb21a41e9a168
fof3096 <c0rr1f1ll@gmail.com>
2024-11-20 17:39:40 -0300

    fix infoBar text colors

--- src/components/InfoBar.tsx
+++ src/components/InfoBar.tsx
@@ -8,33 +8,33 @@ export default function InfoBar() {
   return (
     <Stack p={2} direction={"row"} justifyContent={"space-evenly"} divider={<Divider orientation="vertical" flexItem sx={{ borderColor: "text.Secondary" }}/>}>
       <Box display={"flex"} alignItems={"center"} gap={1}>
-        <StorefrontIcon color="secondary"/>
+        <StorefrontIcon/>
         <Stack spacing={-1}>
-          <Typography color="secondary" fontWeight={"bold"}>RETIRO GRATIS</Typography>
+          <Typography fontWeight={"bold"}>RETIRO GRATIS</Typography>
           <Typography color="textSecondary" fontSize={"small"} >en sucursal</Typography>
         </Stack>
       </Box>
 
       <Box display={"flex"} alignItems={"center"} gap={1}>
-        <LocalShippingOutlinedIcon color="secondary"/>
+        <LocalShippingOutlinedIcon/>
         <Stack spacing={-1}>
-          <Typography color="secondary" fontWeight={"bold"}>ENVÍOS A DOMICIOLIO</Typography>
+          <Typography fontWeight={"bold"}>ENVÍOS A DOMICIOLIO</Typography>
           <Typography color="textSecondary" fontSize={"small"}>en toda la provincia</Typography>
         </Stack>
       </Box>
 
       <Box display={"flex"} alignItems={"center"} gap={1}>
-        <CreditCardIcon color="secondary"/>
+        <CreditCardIcon/>
         <Stack spacing={-1}>
-          <Typography color="secondary" fontWeight={"bold"}>PROMOS A BANCARIAS</Typography>
+          <Typography fontWeight={"bold"}>PROMOS A BANCARIAS</Typography>
           <Typography color="textSecondary" fontSize={"small"}>y medios de pago</Typography>
         </Stack>
       </Box>
 
       <Box display={"flex"} alignItems={"center"} gap={1}>
-        <WhatsAppIcon color="secondary"/>
+        <WhatsAppIcon/>
         <Stack spacing={-1}>
-          <Typography color="secondary" fontWeight={"bold"}>223 3393007</Typography>
+          <Typography fontWeight={"bold"}>223 3393007</Typography>
           <Typography color="textSecondary" fontSize={"small"}>consultanos</Typography>
         </Stack>
       </Box>

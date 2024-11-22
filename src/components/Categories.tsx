255d1b1777efd221bd14b9535fd4f172ecf30a2e
fof3096 <c0rr1f1ll@gmail.com>
2024-11-22 11:18:13 -0300

    Categories effect

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -1,9 +1,15 @@
 import { Box, Grid2, styled, Typography } from "@mui/material";
 
-const BookImg = styled('img')(()=>({
+const BookImg = styled('img')(({theme})=>({
     objectFit: "cover",
     width: "100%",
-  }))
+    transition: theme.transitions.create("transform", {
+      duration: theme.transitions.duration.short,
+    }),
+    "&:hover": {
+      transform: "scale(1.1)"
+    }
+}))
 
 const imgUrls = ["https://www.penguinlibros.com/ar/img/cms/Argentina/Literatura-1.jpg", "https://www.penguinlibros.com/ar/img/cms/Argentina/Tematicas-Bienestar-1.webp", "https://www.penguinlibros.com/ar/img/cms/Argentina/Tematicas-Familia-1.webp", "https://www.penguinlibros.com/ar/img/cms/Argentina/Tematicas-Cocina-1.webp", "https://www.penguinlibros.com/ar/img/cms/Argentina/infantil_.jpg", "https://www.penguinlibros.com/ar/img/cms/Argentina/Tematicas-Clasicos-1.webp", "https://www.penguinlibros.com/ar/img/cms/Argentina/Novela-Romantica-1.jpg", "https://www.penguinlibros.com/ar/img/cms/Argentina/fantasia_.jpg"]
 const titles = ["Literatura", "Bienestar", " Familia y crianza", "Cocina", "Infantil", "Grandes clásicos", "Románticas", "Aventura"]
@@ -16,9 +22,9 @@ export default function Categories() {
           <Box position={"absolute"} top={"-15%"} left={"50%"} height={"110%"} width={"100vw"} zIndex={-1} bgcolor={"secondary.main"} sx={{ transform: "translateX(-50%)" }}></Box>
           {
               imgUrls.map((url, i)=>(
-                  <Grid2 key={i} width={"25%"} position={"relative"}>
-                      <Typography variant="h5" textAlign={"center"} bgcolor={"background.default"} position={"absolute"} width={"80%"} left={"50%"} top={15} sx={{ transform: "translateX(-50%)" }}>{titles[i]}</Typography>
-                      <BookImg src={url}/>
+                  <Grid2 key={i} width={"25%"} position={"relative"} overflow={"hidden"}>
+                      <Typography variant="h5" textAlign={"center"} bgcolor={"background.default"} position={"absolute"} zIndex={3} width={"80%"} left={"50%"} top={15} sx={{ transform: "translateX(-50%)" }}>{titles[i]}</Typography>
+                      <BookImg src={url} />
                   </Grid2>
               ))
           }
